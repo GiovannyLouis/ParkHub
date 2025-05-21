681faad4044a07d432bf62dace7ca0ef1d9ef488
GiovannyLouis <louis.anth10@gmail.com>
2025-05-21 14:21:44 +0700

    Initial Commit

---
+++ ParkHub/ParkHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkHubApp.swift
+//  ParkHub
+//
+//  Created by student on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ParkHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParkHubTests/ParkHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParkHubTests.swift
+//  ParkHubTests
+//
+//  Created by student on 21/05/25.
+//
+
+import Testing
+@testable import ParkHub
+
+struct ParkHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
