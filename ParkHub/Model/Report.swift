c44f2ec2e9f634d3f4edee2d4d5f6cd24c3b527a
MattGabs <charlenlaw06@gmail.com>
2025-05-30 10:10:41 +0700

    renew reportModel

--- ParkHub/Model/Report.swift
+++ ParkHub/Model/Report.swift
@@ -1,1 +1,9 @@
 import Foundation
+
+struct Report {
+    var reportId: String = ""
+    var userId: String = ""
+    var title: String = ""
+    var description: String = ""
+    var imageUrl: String = ""
+}
